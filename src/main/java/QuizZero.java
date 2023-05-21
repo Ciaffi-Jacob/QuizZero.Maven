bc740fe99d5a43bad1afa6fcd3c30c6c25c60ee9
Jacob C <jacobc@Zipcoders-MacBook-Pro-3.local>
2023-05-20 20:00:42 -0400

    tests passed

--- src/main/java/QuizZero.java
+++ src/main/java/QuizZero.java
@@ -46,17 +46,37 @@ public class QuizZero {
     //return the sum of a and b divded by c
     public static  int Qeight(int a, int b, int c) { return ((a + b) / c); }
     // write code to pass the test
-    public static  int Qnine(int a, int b) { return 0; }
+    public static  int Qnine(int a, int b) { return (a * b + 5); }
     // write code to pass the test
-    public static  boolean Qaay(boolean a, boolean b) { return false; }
+    public static  boolean Qaay(boolean a, boolean b) { 
+        if (a && b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // write code to pass the test
-    public static  boolean Qbee(boolean a, boolean b) { return false; }
+    public static  boolean Qbee(boolean a, boolean b) { 
+        if(a || b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     // write code to pass the test
-    public static  int Qcee(boolean a, boolean b) { return 0; }
+    public static  int Qcee(boolean a, boolean b) { 
+        if(a == b){
+            return -2;
+        } else {
+            return 1;
+        } }
     //return x dvided by b squared
-    public static  float Qdee(float x, int b) { return 0.0f; }
+    public static  float Qdee(float x, int b) { 
+        return (x / (b * b)); }
     // if x-y is less than 1/10,000 return zero otherwise return 1
-    public static  int Qeee(double x, double y) { return 0; }
+    public static  int Qeee(double x, double y) { 
+        return Math.abs(x - y) < .00001 ? 0 : 1;
+    }
     //if a is zero return zero, if a is even return a random number times zero, if not even return 5 - 5
     public static  int Qeff(int a) { return 0; }
 
